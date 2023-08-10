7b113e3191e7053f1d747717312b9d272d47b3dd
isinhag <isabeladasilvagomes@gmail.com>
2023-08-10 14:29:40 -0300

    PHPAula1
    Aula 1- Sintaxe e servidor local com XAMPP

---
+++ Aula01/sintax1.php
@@ -0,0 +1,9 @@
+<?php
+    // Geração de texto (stargin)
+    echo "Chama Sesi-Senai";
+
+    /* Geração de texto estruturado (com tags, atributos) */
+    echo "<h2>Gerando <span class='destaque'>HTML</span> através do PHP.</h2>";
+    echo "<h2>Gerando <span class=\"destaque\">HTML</span> através do PHP.</h2>";
+
+?>
