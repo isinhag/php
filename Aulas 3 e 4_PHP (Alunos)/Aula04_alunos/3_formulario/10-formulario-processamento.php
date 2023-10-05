d043689e4a26e76a34da24e8d8299e497c95f5ab
isinhag <isabeladasilvagomes@gmail.com>
2023-10-05 11:40:05 -0300

    .

--- Aulas 3 e 4_PHP (Alunos)/Aula04_alunos/3_formulario/10-formulario-processamento.php
+++ Aulas 3 e 4_PHP (Alunos)/Aula04_alunos/3_formulario/10-formulario-processamento.php
@@ -15,7 +15,26 @@
     <!-- ________________________________________________________________________________ -->
  
 
-    <!-- 1ª Digitação (aqui) -->
+    <?php
+    //Detectar quando o formulário é acionado
+        if (isset($_POST['enviar']) ) {
+       
+            // Validando se os campos foram preenchidos 
+            if(empty($_POST['nome']) || empty($_POST['email']) ) {
+
+    ?>
+        <!-- Se campos vazios solicita ao usuário o preenchimento -->
+        <p style="color:red">Preencher os campos</p>
+        <p><a href=""><link href="10-formulário-processamento.php">Voltar</a></p>
+
+    <?php
+        } else {
+            $nome = filter_input (INPUT_POST, "nome", FILTER_SANITIZE_SPECIAL_CHARS);
+            $email = filter_input (INPUT_POST, "email", FILTER_SANITIZE_EMAIL);
+      
+    ?>    
+
+
 
     <section>
         <h2>Dados</h2>
@@ -26,7 +45,12 @@
     <p><a href=""><link href="10-formulario-processamento.php">Voltar</a></p>
     
 
-    <!-- 2ª Digitação (aqui) -->
+    <?php
+        }
+
+    } else {
+
+    ?>
 
     
     <!-- Entra aqui porque ao entrar na página ele entra no falso e cai aqui (Mostra formulário) -->
@@ -45,7 +69,10 @@
         <button type="reset" name="limpar">Limpar</button>
     </form>
 
-    <!-- 3ª Digitação (aqui) -->
+    <?php
+        }
+
+    ?>
     
 </body>
 </html>
--- Aulas 3 e 4_PHP (Alunos)/Aula04_alunos/4_formulario_V2/processa-post.php
+++ Aulas 3 e 4_PHP (Alunos)/Aula04_alunos/4_formulario_V2/processa-post.php
@@ -10,9 +10,54 @@
     <h1>Processamento de dados usando POST</h1>
     <hr>
     <!-- Método mais seguro (Não mostra as informações na URL) -->
- 
+    <?php  
+        // SE + OU (|| (Quando o usuário não preencher nome e-mail))
+        if(empty($_POST['nome']) || empty($_POST['email']) ) {
+    ?>
+    <p style="color:red">Você deve preencher nome e e-mail!</p>
+    
+    <?php 
+    } else {
 
+        // Captura dos dados com filtro de segurança 
+            $nome = filter_input(INPUT_POST, 'nome', FILTER_SANITIZE_SPECIAL_CHARS);
+            $email = filter_input(INPUT_POST, 'email', FILTER_SANITIZE_EMAIL);
+            $idade = filter_input(INPUT_POST, 'idade', FILTER_SANITIZE_NUMBER_INT);
+            $mensagem = filter_input(INPUT_POST, 'mensagem', FILTER_SANITIZE_SPECIAL_CHARS);
+            $informativos = filter_input(INPUT_POST, 'informativos', FILTER_SANITIZE_SPECIAL_CHARS);
 
+        // Operadores de coalescência nula (Verifica se o que está a esquerda existe (intereses), senão pega o da direita, neste caso vazio [])
+        $interesses = filter_var_array($_POST ['interesses'] ?? [], FILTER_SANITIZE_FULL_SPECIAL_CHARS);
+
+        $escolha = filter_input(INPUT_POST, 'escolha', FILTER_SANITIZE_FULL_SPECIAL_CHARS);
+
+    ?> 
+    <h2>Dados</h2>
+
+    <ul>
+        <li>Nome: <?=$nome?></li>
+        <li>E-mail: <?=$email?></li>
+        <li>Idade: <?=$idade?></li>
+        <li>Informativos: <?=$informativos?></li>
+
+        <!-- _________________________________________________ -->
+
+        <li>Interesses:
+            <ul>
+                <!-- Opção 2: Acessar o array usando Loop -->
+                <?php foreach($interesses as $interesse) { ?>
+                    <li> <?=$interesse?> </li>
+                <?php } ?>
+            </ul>
+        </li>
+    <?php } ?>
+                <li>Escolha: <?$escolha?></li>
+                <li>Mensagem: <?$mensagem?></li>
+    </ul>
+
+    <?php 
+        // fim if/else da validação de campos obrigatórios 
+    ?>
 
     
 </body>
